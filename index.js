468f0e78059413958fb017880007a81a6f84c4b1
danon <pro0@nortcele.win>
2019-02-25 17:41:23 +1100

    spacing

--- index.js
+++ index.js
@@ -10,21 +10,19 @@ var options = {
     port: 5269,
     clientId: 'avis'
 }
-var client = mqtt.connect(options);
+var MQTTclient = mqtt.connect(options);
 
 // Loading assert module - Error Checking
 const assert = require('assert');
 
-// Loading moment module - To get time stamp
-// var moment = require('moment');
-
 // Loading MongodB Client module
 var MongoClient = require('mongodb').MongoClient;
+
 // MongoDB database name 
 const dbName = 'Canary';
+
 // MongoDB connection url 
 var MongoDB_url = "mongodb://bananas_and_dingoes.summerstudio.xyz:27017";
-
 
 //-----------------------------------------------------------------------------
 // Connecting to the Database and selecting topic/data
@@ -48,7 +46,7 @@ MongoClient.connect(MongoDB_url, {useNewUrlParser: true}, function(error, initia
 	const Ibis_collection    	= db.collection('Ibis');			// Rachels Collection
 
 	// When message recieved via topic, excute function
-	client.on('message', function (topic, message) {
+	MQTTclient.on('message', function (topic, message) {
 		// Message received over Canary subscription
 		var Cdata = Buffer.from(message);
 		// Convert message to string
@@ -91,12 +89,12 @@ MongoClient.connect(MongoDB_url, {useNewUrlParser: true}, function(error, initia
 			// setting time between 10 pm and 6 am so that device will sleep for an hour.
 			if ((today +11) >= 22 && (today+ 11) < 6) {
 				
-			client.publish('ChairMon/Return', "LateRecieved");
+			MQTTclient.publish('ChairMon/Return', "LateRecieved");
 			} 
 			
 			//Any other time, device will sleep for 5 min.
 			else {
-				client.publish('ChairMon/Return', "Recieved");
+				MQTTclient.publish('ChairMon/Return', "Recieved");
 			}
 		}
 
@@ -109,15 +107,15 @@ MongoClient.connect(MongoDB_url, {useNewUrlParser: true}, function(error, initia
 //-----------------------------------------------------------------------------
 
 // When connected subscribe to desired topic. Currently to Canary/+
-client.on('connect', function () {
+MQTTclient.on('connect', function () {
 
 	console.log("Connected to the MQTT Broker");
-	client.subscribe('Canary/+', function (error) {	
+	MQTTclient.subscribe('Canary/+', function (error) {	
 	
-	if (!error)
-		console.log("Subscribed to Canary/+ Topic ");
-	else
-		console.log("Cannot subscribe, server won't allow");
+		if (!error)
+			console.log("Subscribed to Canary/+ Topic ");
+		else
+			console.log("Cannot subscribe, server won't allow");
 
 	});
 });
